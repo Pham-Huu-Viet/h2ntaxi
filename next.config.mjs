280ac74f81b454b9d68d7fdef0a76ce599e9c722
Pham-Huu-Viet <phamhuuviet1998cx@gmail.com>
2026-04-09 14:21:34 +0700

    add rank A security

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,42 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            // Cấu hình này cho phép: Google Analytics, Vercel, Inline Scripts và Google Fonts
+            value:
+              "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' *.googletagmanager.com va.vercel-scripts.com; style-src 'self' 'unsafe-inline' fonts.googleapis.com; font-src 'self' fonts.gstatic.com data:; img-src 'self' data: *.googletagmanager.com *.google-analytics.com; connect-src 'self' *.google-analytics.com *.analytics.google.com *.googletagmanager.com va.vercel-scripts.com;",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "origin-when-cross-origin",
+          },
+          {
+            key: "Permissions-Policy",
+            value:
+              "camera=(), microphone=(), geolocation=(), interest-cohort=()",
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=31536000; includeSubDomains; preload",
+          },
+        ],
+      },
+    ];
+  },
   async redirects() {
     return [
       {
@@ -18,3 +54,26 @@ const nextConfig = {
 };
 
 export default nextConfig;
+
+// // next.config.mjs
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   images: {
+//     unoptimized: true,
+//   },
+//   async redirects() {
+//     return [
+//       {
+//         source: "/",
+//         destination: "/thue-xe-hop-dong",
+//         permanent: true,
+//       },
+//     ];
+//   },
+// };
+
+// export default nextConfig;
